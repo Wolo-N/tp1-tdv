b099eee7cc644c8ce33cd78b8f224ab86991a207
Wolo-N <nicolas.wolodarskyfranke@gmail.com>
2024-04-20 22:39:34 -0300

    fuerza bruta LISTO

--- src/python/fuerzaBruta.py
+++ src/python/fuerzaBruta.py
@@ -8,7 +8,7 @@ from shared import calcular_error, plot_graph
 def fuerza_bruta_recursiva(m, n, N, instance, i, bp, error_total, combinaciones, grid_x, grid_y):
     """
     Función recursiva para encontrar todas las combinaciones de breakpoints y calcular el error total para cada combinación.
-    
+
     Parámetros:
         - m (int): Número de puntos en la grilla a lo largo del eje x.
         - n (int): Número de puntos en la grilla a lo largo del eje y.
@@ -20,7 +20,7 @@ def fuerza_bruta_recursiva(m, n, N, instance, i, bp, error_total, combinaciones,
         - combinaciones (dict(lista de tuplas, error asociado)): Diccionario para almacenar las combinaciones de breakpoints y sus errores totales.
         - grid_x (numpy.ndarray): Coordenadas x de la grilla.
         - grid_y (numpy.ndarray): Coordenadas y de la grilla.
-    
+
     Devuelve:
         combinaciones (dict(lista de tuplas, error asociado)): Diccionario con todas las combinaciones posibles y su error asociado
     """
@@ -30,7 +30,7 @@ def fuerza_bruta_recursiva(m, n, N, instance, i, bp, error_total, combinaciones,
         # Registra la combinación actual y su error total.
         combinaciones[tuple(bp)] = round(error_total, 3)
         return bp, error_total, combinaciones
-    
+
     # Si es el primer breakpoint, llama recursivamente sobre todas las f(x), sin añadir error.
     if not bp:
         for z in range(m):
@@ -38,7 +38,7 @@ def fuerza_bruta_recursiva(m, n, N, instance, i, bp, error_total, combinaciones,
             fuerza_bruta_recursiva(m, n, N, instance, 0, new_bp, error_total, combinaciones, grid_x, grid_y)
 
     # Si ya hay breakpoints, pero menos a los esperados (N):
-    elif len(bp) < N: 
+    elif len(bp) < N:
         # Itera sobre todas las filas y columnas para analizar c/combinación posible.
         for j in range(m):
             for k in range(i+1,n):
@@ -54,7 +54,7 @@ def fuerza_bruta_recursiva(m, n, N, instance, i, bp, error_total, combinaciones,
 
 def fuerza_bruta(m, n, N, instance):
     """
-    Realiza una búsqueda exhaustiva para encontrar todas las combinaciones posibles de breakpoints en una grilla y calcula el error total para cada combinación.
+    Pasa parametros de uso a fuerza_bruta_recursiva y ordena su respuesta.
 
     Parámetros:
         m (int): Número de puntos en la grilla a lo largo del eje x.
@@ -68,13 +68,15 @@ def fuerza_bruta(m, n, N, instance):
     # Crea la grilla.
     grid_x = np.linspace(min(instance["x"]), max(instance["x"]), num=m, endpoint=True)
     grid_y = np.linspace(min(instance["y"]), max(instance["y"]), num=n, endpoint=True)
+
     # Crea el diccionario con las soluciones factibles.
     combinaciones = {}
+
     fuerza_bruta_recursiva(m, n, N, instance, 0, [], 0, combinaciones, grid_x, grid_y)
 
     # Ordenamos el diccionario para devolver las mejores 5, según menor error.
     top_combinaciones = sorted(combinaciones.items(), key=lambda item: item[1])[:5]
-    
+
     print(f"Top 5 Combinaciones de {len(combinaciones)}:")
     for idx, (comb, error) in enumerate(top_combinaciones, 1):
         print(f"{idx}: {comb} con error: {error}")
@@ -97,19 +99,22 @@ def fuerza_bruta(m, n, N, instance):
 def main():
     files = ['optimistic_instance.json'] #'aspen_simulation.json', 'ethanol_water_vle.json', 'titanium.json', 'optimistic_instance.json', 'toy_instance.json'
     reps = 1
+
+    # Usamos un loop para correr todas las instancias.
     for filename in files:
         # Carga los datos desde la instancia JSON.
         instance_name = filename
         filename = "data/" + instance_name
         with open(filename) as f:
             instance = json.load(f)
-        # Define los parámetros de la grilla y los breakpoints.
-        for w in range(6,10):
 
+        # Usamos un loop para correr varios tests.
+        for w in range(6,10):
+        # Define los parámetros de la grilla y los breakpoints.
             m = 10
             n = 10
             N = w
-            
+
             for i in range(reps):
                 start_time = time.time()
 
@@ -119,15 +124,14 @@ def main():
                 end_time = time.time()
                 excecution_time = end_time - start_time
                 total_excecution_time =+ excecution_time
-            
+            # Calculamos el tiempo promedio de ejecucion.
             average_excecution_time = total_excecution_time/reps
 
-            # Asegúrate de que el directorio exista
             solution_directory = 'data/solutions'
             if not os.path.exists(solution_directory):
                 os.makedirs(solution_directory)
 
-            # Exporta la solución a un archivo JSON
+            # Exportamos la solución a un archivo JSON
             solution_filename = os.path.join(solution_directory, f'solution_{instance_name}')
             try:
                 with open(solution_filename, 'w') as f:
@@ -136,8 +140,7 @@ def main():
             except Exception as e:
                 print(f"Error al guardar la solución: {e}")
 
-            # Gráfica la solución.
-            print(w)
+            # Gráfica y guarda la solución.
             plot_graph(instance_name, m, n, N, average_excecution_time, min_error, 'Fuerza Bruta', w)
 
 if __name__ == "__main__":
